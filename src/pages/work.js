a763bee5f910e8f4ab801c7df677bbc35ca4aba1
Eric <Aerelioyanadel@gmail.com>
2019-12-09 13:56:28 -0500

    update proper href links to images

--- src/pages/work.js
+++ src/pages/work.js
@@ -12,11 +12,11 @@ class Work extends React.Component{
         <h3 className="work-title"> Past Web Projects</h3>
         <div className="work-image-container">
           <div>
-            <img src={Shipship} alt="shiphip"></img>
+            <a href="https://wyn-shipship.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={Shipship} alt="shiphip"></img></a>
               <p> Site displays understandment of REST from the EasyPost API and demonstrates a solid foundation of a Rails database of user information.</p>
           </div>
           <div>
-            <img src={Barhop} alt="shiphip"></img>
+            <a href="http://barhop-wyncode.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={Barhop} alt="shiphip"></img></a>
             <p>Team of three developers to create information of nearest bars withing user location entered. shows good use of REST with the Yelp API. Two week project</p>
           </div>
         </div>
