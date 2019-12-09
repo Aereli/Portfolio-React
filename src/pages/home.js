ad534d9aff9ba975e0ddae143c52161dff0cbba6
Eric <Aerelioyanadel@gmail.com>
2019-12-08 21:32:28 -0500

    set image on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom' 
+import '../stylesheets/home.css'
+import Face from '../images/face_black.svg'
 
 class Home extends React.Component{
   render(){
     return(
       <div>
-        <h1> this is the home page</h1>
+        <Router>
+          <h1><Link to="./home.js" className="home-header">Oyanadel</Link></h1>
+          <Link to="./work.js" className="home-nav-link">Work</Link>
+        </Router>
+        <div>
+          <img src={Face} alt="image" className="home-face-image" />
+        </div> 
+        <footer className="home-footer">
+            © {new Date().getFullYear()}, from scratch by Eric
+              {` `}
+        </footer>
       </div>
     )
   }
