4b66720834455392fe8850d68ad1fde0d91b3e35
Eric <Aerelioyanadel@gmail.com>
2019-12-09 13:33:13 -0500

    added responsive grip for images

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../stylesheets/work.css'
+import Shipship from '../images/shipship.png'
+import Barhop from '../images/barhop.png'
 
 class Work extends React.Component{
   render(){
@@ -7,8 +9,11 @@ class Work extends React.Component{
       <div>
         <h1 className="work-header"><a href ="/">Oyanadel</a></h1>
 
-        <h1 >this is the work page</h1>
-
+        <h3 className="work-title"> Past Web Projects</h3>
+        <div className="work-image-container">
+          <img src={Shipship} alt="shiphip"></img>
+          <img src={Barhop} alt="shiphip"></img>
+        </div>
       </div>
     )
   }
