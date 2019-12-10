6ca60c9fe79a69f13432bebd224ea3b9bc396c28
Eric <Aerelioyanadel@gmail.com>
2019-12-10 14:14:56 -0500

    followed netlify docs to add Form Handling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,23 @@ class Contact extends React.Component{
   render(){
     return(
       <>
-       <h1> this is the contact page</h1>
+        <h1>contact</h1>
+        <p> Send me a message!</p>
+        <form name="contact" method="post" action="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </>
     )
   }
