9e9e00edd9a79605ca9e7f50f1f551ac911fd1fe
Eric <Aerelioyanadel@gmail.com>
2019-12-10 13:49:53 -0500

    initial base from contact page created

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Contact extends React.Component{
+  render(){
+    return(
+      <>
+       <h1> this is the contact page</h1>
+      </>
+    )
+  }
+
+}
+
+export default Contact
