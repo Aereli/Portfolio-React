e1ae4302bb1bb9e811665233a5cc4076711cdde2
Eric <Aerelioyanadel@gmail.com>
2019-12-11 14:02:55 -0500

    added effeccts to global css in index.css

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,14 +3,26 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import '../stylesheets/home.css'
 import Face from '../images/face_black.svg'
 
+
 class Home extends React.Component{
+  
   render(){
+    const string = "Oyanadel"
+    const splitstring = string.split("")
     return(
       <div>
         <Router>
-            <h1 className="home-header"><a href ="/">Oyanadel</a></h1>
-            <h2 className="home-nav-link"><a href ="/work">work</a></h2>
-            <h2 className="home-nav-link"><a href ="/contact">contact</a></h2>
+    <h1 className="home-header">
+      <a href="/">
+        {
+          splitstring.map(letter => (
+            <span className="each-letter">{letter}</span>
+          ))
+        }
+      </a>
+      </h1>
+          <h2 className="home-nav-link"><a href="/work">work</a></h2>
+          <h2 className="home-nav-link"><a href="/contact">contact</a></h2>
         </Router>
         <div>
           <img src={Face} alt="face" className="home-face-image" />
