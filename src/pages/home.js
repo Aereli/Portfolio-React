f0da32968859c824945b03ff170ef0257b93674e
Eric <Aerelioyanadel@gmail.com>
2019-12-12 16:10:41 -0500

    added links to footer with layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router} from 'react-router-dom'
 import '../stylesheets/home.css'
 // import Face from '../images/face_black.svg'
-import Face2 from '../images/face_three_parts.svg'
-// import part1 from '../images/part1.svg'
-// import part2 from '../images/part2.svg'
-// import part3 from '../images/part3.svg'
+import Face from '../images/face_three_parts.svg'
+import Github from '../images/github-brands.svg'
+import Linkedin from '../images/linkedin.svg'
 
 class Home extends React.Component{
   
@@ -15,15 +14,15 @@ class Home extends React.Component{
     return(
       <div>
         <Router>
-    <h1 className="home-header">
-      <a href="/">
-        {
-          splitstring.map(letter => (
-            <span className="each-letter">{letter}</span>
-          ))
-        }
-      </a>
-      </h1>
+          <h1 className="home-header">
+            <a href="/">
+              {
+                splitstring.map(letter => (
+                  <span className="each-letter">{letter}</span>
+                ))
+              }
+            </a>
+          </h1>
         <div className="nav-link-container">
           <h2 className="home-nav-link">
             <a href="/work">work</a>
@@ -34,9 +33,13 @@ class Home extends React.Component{
         </div>
         </Router>
         <div>
-          <img src={Face2} alt="face" className="home-face-image"/>
+          <img src={Face} alt="face" className="home-face-image"/>
         </div> 
         <footer className="home-footer">
+          <div>
+            <a href="https://www.linkedin.com/in/eric-oyanadel/" ><img src={Linkedin} alt="face" className="footer-github"/></a>
+            <a href="https://github.com/Aereli" ><img src={Github} alt="face" className="footer-github"/></a>
+          </div>
             © {new Date().getFullYear()}, from scratch by Eric
               {` `}
         </footer>
