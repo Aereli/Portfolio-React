79e1f923c454cfbe277a18575522ca65728093ac
Eric <Aerelioyanadel@gmail.com>
2019-12-20 10:10:38 -0500

    added correct links to back of cards

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,7 +3,7 @@ import '../stylesheets/work.css'
 import Shipship from '../images/shipship.png'
 import Barhop from '../images/barhop.png'
 import Splttr from '../images/splttr.png'
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 
 class Work extends React.Component{
@@ -26,14 +26,13 @@ class Work extends React.Component{
         <div className="work-image-container">
           <div className="card">
             <div className="front">
-            <a href="https://wyn-shipship.herokuapp.com/" target="_blank" rel="noopener noreferrer">
               <img className="work-projects" src={Shipship} alt="shiphip"></img>
-            </a>
             </div>
             <div className="back">
               <div className="back-content-middle">
                 <p> Site displays understandment of REST concept from the EasyPost API and a foundation of a Rails database of user information.</p>
                 <div className="work-github">
+                  <a href="https://wyn-shipship.herokuapp.com/" target="_blank" rel="noopener noreferrer"><FaLink className="footer-github"/></a>
                   <a href="https://github.com/Aereli/ShipShip" target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
                 </div>
               </div>
@@ -42,14 +41,13 @@ class Work extends React.Component{
 
           <div className="card">
             <div className="front">
-              <a href="http://barhop-wyncode.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                 <img className="work-projects" src={Barhop} alt="Barhop"></img>
-              </a>
             </div>
             <div className="back">
               <div className="back-content-middle">
                 <p>Team of three developers to create information of nearest bars within user location entered. shows good use of REST with the Yelp API. Two week project</p>
                 <div className="work-github">
+                  <a href="http://barhop-wyncode.herokuapp.com/" target="_blank" rel="noopener noreferrer"><FaLink className="footer-github"/></a>
                   <a href="https://github.com/Aereli/Bar_hop_final" target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
                 </div>
               </div>
@@ -58,14 +56,13 @@ class Work extends React.Component{
           
           <div className="card">
             <div className="front">
-              <a href="https://aereli.github.io/Tip-Calculator/" target="_blank" rel="noopener noreferrer">
-                <img className="work-projects" src={Splttr} alt="Splttr" ></img>
-              </a>
+                <img className="work-projects" src={Splttr} alt="Splttr" ></img>          
             </div>
             <div className="back">
               <div className="back-content-middle">
                 <p>Nice little Tip calculator with great example of Readline Sync</p>
                 <div className="work-github">
+                  <a href="https://aereli.github.io/Tip-Calculator/" target="_blank" rel="noopener noreferrer"><FaLink className="footer-github"/></a>
                   <a href="https://github.com/Aereli/Tip-Calculator"target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
                 </div>
               </div>
