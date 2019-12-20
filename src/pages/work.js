d818941cd746b95a23aa81199929d467a35b55c8
Eric <Aerelioyanadel@gmail.com>
2019-12-20 09:47:53 -0500

    fixed card flip on work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -25,32 +25,54 @@ class Work extends React.Component{
         <h3 className="work-title"> Past Web Projects</h3>
         
         <div className="work-image-container">
-          <div>
-            <a href="https://wyn-shipship.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img className="work-projects" src={Shipship} alt="shiphip"></img></a>
-              <p> Site displays understandment of REST from the EasyPost API and demonstrates a solid foundation of a Rails database of user information.</p>
-              <div className="work-github">
-                <a href="https://github.com/Aereli/ShipShip" target="_blank" rel="noopener noreferrer" ><img src={Github} alt="github-icon"></img></a> 
-              </div>
-          </div>
-          <div>
-            <a href="http://barhop-wyncode.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-              <img className="work-projects" src={Barhop} alt="Barhop"></img>
+          <div className="card">
+            <div className="front">
+            <a href="https://wyn-shipship.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <img className="work-projects" src={Shipship} alt="shiphip"></img>
             </a>
-            <p>Team of three developers to create information of nearest bars within user location entered. shows good use of REST with the Yelp API. Two week project</p>
-            <div className="work-github">
-              <a href="https://github.com/Aereli/Bar_hop_final" target="_blank" rel="noopener noreferrer"><img src={Github} alt="github-icon"></img></a> 
             </div>
-          </div>
-          <div className="image-container">
-            <a href="https://aereli.github.io/Tip-Calculator/" target="_blank" rel="noopener noreferrer">
-              <img className="work-projects" src={Splttr} alt="Splttr" ></img>
-              <p>Nice little Tip calculator with great example of Readline Sync</p>
-            </a>
-            <div className="work-github">
-              <a href="https://github.com/Aereli/Tip-Calculator" target="_blank" rel="noopener noreferrer"><img src={Github} alt="github-icon"></img></a> 
+            <div className="back">
+              <div className="back-content-middle">
+                <p> Site displays understandment of REST concept from the EasyPost API and a foundation of a Rails database of user information.</p>
+                <div className="work-github">
+                  <a href="https://github.com/Aereli/ShipShip" target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
+                </div>
+              </div>
             </div>
           </div>
 
+          <div className="card">
+            <div className="front">
+              <a href="http://barhop-wyncode.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                <img className="work-projects" src={Barhop} alt="Barhop"></img>
+              </a>
+            </div>
+            <div className="back">
+              <div className="back-content-middle">
+                <p>Team of three developers to create information of nearest bars within user location entered. shows good use of REST with the Yelp API. Two week project</p>
+                <div className="work-github">
+                  <a href="https://github.com/Aereli/Bar_hop_final" target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <div className="front">
+              <a href="https://aereli.github.io/Tip-Calculator/" target="_blank" rel="noopener noreferrer">
+                <img className="work-projects" src={Splttr} alt="Splttr" ></img>
+              </a>
+            </div>
+            <div className="back">
+              <div className="back-content-middle">
+                <p>Nice little Tip calculator with great example of Readline Sync</p>
+                <div className="work-github">
+                  <a href="https://github.com/Aereli/Tip-Calculator"target="_blank" rel="noopener noreferrer" ><FaGithub className="footer-github"/></a>
+                </div>
+              </div>
+            </div>
+          </div>
+{/* 
           <div className="card">
             <div className="front">
               <img src={Splttr} alt="Splttr" ></img>
@@ -64,8 +86,8 @@ class Work extends React.Component{
                 </div>
               </div>
             </div>
+          </div> */}
 
-          </div>
         </div>
       </div>
     )
