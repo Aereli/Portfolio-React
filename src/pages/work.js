e1625187e9ea62338faa86414f4db5f15b533b60
Eric <Aerelioyanadel@gmail.com>
2020-03-21 22:16:04 -0400

    update work with new site

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,7 +3,12 @@ import "../stylesheets/work.css"
 import Shipship from "../images/shipship.png"
 import Barhop from "../images/barhop.png"
 import Splttr from "../images/splttr.png"
+import AereliArt from "../images/aereliArt.png"
 import { FaGithub, FaLink } from "react-icons/fa"
+
+import github from "../images/github-brands.svg"
+import linkedin from "../images/linkedin.svg"
+
 // import SplitString from "../components/stringSplitter"
 import Navbar from "../components/Nav"
 
@@ -37,14 +42,48 @@ class Work extends React.Component {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaLink className="footer-github" />
+                    <img src={linkedin} alt="linkedin" />
                   </a>
                   <a
                     href="https://github.com/Aereli/ShipShip"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaGithub className="footer-github" />
+                    <img src={github} alt="github" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="front">
+              <img
+                className="work-projects"
+                src={AereliArt}
+                alt="aereliArt"
+              ></img>
+            </div>
+            <div className="back">
+              <div className="back-content-middle">
+                <p>Gatsby</p>
+                <p>React</p>
+                <p>Graphql</p>
+                <p> SCSS</p>
+                <div className="work-github">
+                  <a
+                    href="https://www.aereli.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={linkedin} alt="linkedin" />
+                  </a>
+                  <a
+                    href="https://github.com/Aereli/AereliArt"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={github} alt="github" />
                   </a>
                 </div>
               </div>
@@ -66,14 +105,14 @@ class Work extends React.Component {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaLink className="footer-github" />
+                    <img src={linkedin} alt="linkedin" />
                   </a>
                   <a
                     href="https://github.com/Aereli/Bar_hop_final"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaGithub className="footer-github" />
+                    <img src={github} alt="github" />
                   </a>
                 </div>
               </div>
@@ -95,14 +134,14 @@ class Work extends React.Component {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaLink className="footer-github" />
+                    <img src={linkedin} alt="linkedin" />
                   </a>
                   <a
                     href="https://github.com/Aereli/Tip-Calculator"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaGithub className="footer-github" />
+                    <img src={github} alt="github" />
                   </a>
                 </div>
               </div>
