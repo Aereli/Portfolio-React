01b1d9cd0442f126b481194a1c0d2aff751de60a
Eric <Aerelioyanadel@gmail.com>
2020-03-21 22:14:49 -0400

    update home page iamge

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,16 +1,14 @@
 import React from "react"
-// import { useSpring, animated } from "react-spring"
-// import simpleParallax from "simple-parallax-js"
-// import circle5 from "../images/Ellipse5.svg"
-// import circle4 from "../images/Ellipse4.svg"
-// import { Parallax, ParallaxLayer } from "react-spring/addons"
-// import Parallax from "parallax-js"
+
+// import myFace from "../images/myface 2.png"
+import myFace from "../images/myface 1.svg"
+
 import "../stylesheets/home.css"
 
 const Circles = () => {
   return (
     <>
-      <div class="ball"></div>
+      <img className="my-face-image" src={myFace} alt="face" />
     </>
   )
 }
